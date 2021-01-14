a8416ccde86a5b52f70ce3613fe22e1f28f978a5
leathebest <noamlea74@gmail.com>
2021-01-14 16:15:08 +0200

    added contollers, models and routes

--- src/server.js
+++ src/server.js
@@ -1,25 +1,29 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const dotenv = require("dotenv").config();
-// const mongoose = require("mongoose");
-const PORT = process.env.PORT || 5000;
 
+const PORT = process.env.PORT || 5000;
 const app = express();
+
+/* Routers */
+const userRouter = require("../routes/userRouter");
+const jobRouter = require("../routes/jobRouter");
 
 /* Middleware */
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-
-
 app.use(
   cors({
     origin: "http://localhost:3000", //  Need to change once we deploy the app
     credentials: true,
   })
 );
+
+app.use('/api/users', userRouter);
+app.use('/api/jobs', jobRouter);
 
 app.get("/", (req, res) => {
   try {
@@ -28,8 +32,6 @@ app.get("/", (req, res) => {
     res.status(404).send(error.message);
   }
 });
-
-
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port http://localhost:${PORT}`);
